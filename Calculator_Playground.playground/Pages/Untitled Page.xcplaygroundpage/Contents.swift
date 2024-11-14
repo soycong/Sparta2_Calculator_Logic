55e989fc98382c5f348a061e78b9c93fcf0cdf24
soy <soysauce1223@naver.com>
2024-11-14 14:44:58 +0900

    [REFATOR] 과제 피드백 반영 (operationType enum 추가, 나머지 구하는 클래스 분리 및  remainder -> ModulusOperation로 이름 변경, AbstractOperation 프로토콜 활용)

--- Calculator_Playground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Calculator_Playground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -42,8 +42,10 @@ class DivideOperation: AbstractOperation {
         
         return result
     }
-    
-    func calculateRemainder(_ num1: Double, _ num2: Double) -> Double? {
+}
+
+class ModulusOperation: AbstractOperation {
+    func calculate(_ num1: Double, _ num2: Double) -> Double? {
         // 예외처리) 나누기 진행할 시, 분모가 0이 될 수 없으므로.
         if num2 == 0 {
             return nil
@@ -56,29 +58,29 @@ class DivideOperation: AbstractOperation {
     }
 }
 
+enum OperationType: String {
+    case add, subtract, multiply, divide, mod
+}
+
 // 각 사칙 연산이 진행될 수 있도록 하는 Calculator 클래스
-class Calculator {
-    func operation(_ num1: Double, _ num2: Double, operation: String) -> Double? {
-        switch operation {
-        case "add":
-            return AddOperation().calculate(num1, num2)
-       
-        case "subtract":
-            return SubtractOperation().calculate(num1, num2)
-       
-        case "multiply":
-            return MultiplyOperation().calculate(num1, num2)
+class Calculator {   
+    func calculate(_ num1: Double, _ num2: Double, operationType: OperationType) -> Double? {
+        let operation: AbstractOperation
         
-        case "divide":
-            return DivideOperation().calculate(num1, num2)
-        
-        case "remainder":
-            return DivideOperation().calculateRemainder(num1, num2)
-        
-        default:
-            print("존재하지 않는 연산입니다.") // 더하기, 빼기, 곱하기, 나누기, 나머지 연산이 아닐 경우
-            return nil
+        switch operationType {
+        case .add:
+            operation = AddOperation()
+        case .subtract:
+            operation = SubtractOperation()
+        case .multiply:
+            operation = MultiplyOperation()
+        case .divide:
+            operation = DivideOperation()
+        case .mod:
+            operation = ModulusOperation()
         }
+        
+        return operation.calculate(num1, num2)
     }
 }
 
@@ -88,12 +90,12 @@ let calculator = Calculator()
 let num1 = 5.0
 let num2 = 3.0
 
-let operations = ["add", "subtract", "multiply", "divide", "remainder"]
+let operations: [OperationType] = [.add, .subtract, .multiply, .divide, .mod]
 
 //반복문을 통해 각 연산을 진행
 for operation in operations {
     // 각 연산의 반환값이 옵셔널이므로, 옵셔널 바인딩을 통해 결과값 출력
-    if let result = calculator.operation(num1, num2, operation: operation) {
+    if let result = calculator.calculate(num1, num2, operationType: operation) {
         print("\(operation): \(result)")
     } else {
         print("\(operation): 오류 입니다.")
